f784b76561460ee04505964135e409e2a1fa52f2
Maria del Rosario Arias Nuñez <rosarito_arias15@hotmail.com>
2022-05-27 10:00:48 -0600

    ya quedo

---
+++ vistas/mostrar.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <button type="button">Maria del Rosario</button>
+    <button type="button">Arias Nuñez</button>
+    <button type="button">181000328</button>
+    <button type="button">ProgramacionLogica</button>
+   
+
+</body>
+</html>
